50c051ff9cc73179f600ec074421ab0ec6bcee19
ThiCode126 <59418646+ThiCode126@users.noreply.github.com>
2021-06-23 14:49:06 +0200

    Add Mobile design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section id="home">
+      <div className="content-wrapper">
+        <div className="in-section">
+          <p>Générateur de CSS by </p>
+          <h2>
+            <a
+              href="https://thicode.fr"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              ThiCode
+            </a>
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
